e39f85f1051e48e2c688ce7ac7afae3fac176798
Aubrey Collins <collinsa@allegheny.edu>
2017-11-23 22:56:41 -0500

    Removed unnecessary boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import Interactive from 'react-interactive';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import ExampleComponent from './ExampleComponent';
 import PageNotFound from './PageNotFound';
-import Breadcrumbs from './Breadcrumbs';
-import Navbar from './Navbar';
 import { AdventureHandler } from './Adventure';
-import s from '../styles/app.style';
+import Navbar from './Navbar';
 import { Grid, Row } from 'react-bootstrap';
 
 export default function App() {
   return (
 	<div>
 		<Navbar />
-	<Grid>
-	<Row>
-     <Switch>
-        <Route exact path="/" component={Home} />
-		<Route path="/game/:id" component={AdventureHandler}/>
-        <Route path="/example" component={ExampleComponent} />
-        <Route component={PageNotFound} />
-      </Switch>
-	</Row>
-	</Grid>
+		<Grid>
+			<Row>
+     			<Switch>
+					<Route exact path="/" component={PageNotFound} />
+					<Route path="/game/:id" component={AdventureHandler}/>
+					<Route component={PageNotFound} />
+				</Switch>
+			</Row>
+		</Grid>
     </div>
   );
 }
